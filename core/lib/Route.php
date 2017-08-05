d818eb9df1cc31774baa8e855a8d715fec7e2610
syjs10 <350172898@qq.com>
2017-08-05 16:02:00 +0800

    修改了一些路由可能存在的问题

--- core/lib/Route.php
+++ core/lib/Route.php
@@ -9,16 +9,16 @@
 		 * @return [array] [类,方法与参数]
 		 */
 		function parseURL() {
-			$url = $_SERVER['PATH_INFO'];
+			$url = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
 			$urlArr = explode('/', trim($url,'/'));
 			//多余参数转换成数组
 			for($i = 2; $i < count($urlArr); $i += 2){
 				$data[$urlArr[$i]] = $urlArr[$i + 1];
 			}
 			return array(
-				'ctrl'   => $urlArr[0] ? ucfirst($urlArr[0]) : 'Index',
-				'action' => $urlArr[1] ? $urlArr[1] : 'index',
-				'data'   => $data
+				'ctrl'   => isset($urlArr[0]) && $urlArr[0] ? ucfirst($urlArr[0]) : 'Index',
+				'action' => isset($urlArr[1]) && $urlArr[1] ? $urlArr[1] : 'index',
+				'data'   => isset($data) ? $data : array()
 			);
 
 		}
