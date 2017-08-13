282380f349478861ea48016ec9928c0f51a0c58e
syjs10 <350172898@qq.com>
2017-08-13 15:02:16 +0800

    添加js跳转方式

--- core/Common.php
+++ core/Common.php
@@ -118,6 +118,10 @@
 	    	echo "<script>history.go(-1);</script>";
 
 	    }
+	    function jumps($page){
+	    	echo "<script>window.location.href='$page'</script>";
+
+	    }
 	    /**
 	     * 获取访问Ip地址
 	     * @return [type] [description]
@@ -135,24 +139,24 @@
 		    }  
 		    return $ip;  
 		} 
-		public function _safe($str){
-		    $html_string = array("&amp;", "&nbsp;", "'", '"', "<", ">", "\t", "\r");
-		    $html_clear = array("&", " ", "&#39;", "&quot;", "&lt;", "&gt;", "&nbsp; &nbsp; ", "");
-		    $js_string = array("/<script(.*)<\/script>/isU");
-		    $js_clear = array("");
-		    $frame_string = array("/<frame(.*)>/isU", "/<\/fram(.*)>/isU", "/<iframe(.*)>/isU", "/<\/ifram(.*)>/isU",);
-		    $frame_clear = array("", "", "", "");
-		    $style_string = array("/<style(.*)<\/style>/isU", "/<link(.*)>/isU", "/<\/link>/isU");
-		    $style_clear = array("", "", "");
-		    $str = trim($str);
-		    //过滤字符串
-		    $str = str_replace($html_string, $html_clear, $str);
-		    //过滤JS
-		    $str = preg_replace($js_string, $js_clear, $str);
-		    //过滤ifram
-		    $str = preg_replace($frame_string, $frame_clear, $str);
-		    //过滤style
-		    $str = preg_replace($style_string, $style_clear, $str);
-		    return $str;
-		}    
+		// public function _safe($str){
+		//     $html_string = array("&amp;", "&nbsp;", "'", '"', "<", ">", "\t", "\r");
+		//     $html_clear = array("&", " ", "&#39;", "&quot;", "&lt;", "&gt;", "&nbsp; &nbsp; ", "");
+		//     $js_string = array("/<script(.*)<\/script>/isU");
+		//     $js_clear = array("");
+		//     $frame_string = array("/<frame(.*)>/isU", "/<\/fram(.*)>/isU", "/<iframe(.*)>/isU", "/<\/ifram(.*)>/isU",);
+		//     $frame_clear = array("", "", "", "");
+		//     $style_string = array("/<style(.*)<\/style>/isU", "/<link(.*)>/isU", "/<\/link>/isU");
+		//     $style_clear = array("", "", "");
+		//     $str = trim($str);
+		//     //过滤字符串
+		//     $str = str_replace($html_string, $html_clear, $str);
+		//     //过滤JS
+		//     $str = preg_replace($js_string, $js_clear, $str);
+		//     //过滤ifram
+		//     $str = preg_replace($frame_string, $frame_clear, $str);
+		//     //过滤style
+		//     $str = preg_replace($style_string, $style_clear, $str);
+		//     return $str;
+		// }    
 	}
